c37be9e39ecaddd063bf5d94004b334736d82110
Siddharth Rajit <siddharth.rajit2207@gmail.com>
2026-02-09 19:42:54 -0400

    added more db functionality

---
+++ server/src/drizzle/schemas/departments.ts
@@ -0,0 +1,17 @@
+import { integer, varchar, pgTable, timestamp } from "drizzle-orm/pg-core";
+
+const timestamps = {
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdate(() => new Date())
+    .notNull(),
+};
+
+export const departments = pgTable("departments", {
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  code: varchar("code", { length: 50 }).notNull().unique(),
+  name: varchar("name", { length: 255 }).notNull(),
+  description: varchar("description", { length: 50 }),
+  ...timestamps,
+});
